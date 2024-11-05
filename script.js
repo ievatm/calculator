1ad9ff94d50fd403d41bf578d7d84556e375d902
“ievatm” <Ieva.atmine@gmail.com>
2024-11-05 10:58:39 +0100

    mathematical functions added needet to do the calculation and if divided by 0, give error

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ let secondOperand = "";
 let currentOperator = null;
 let shouldResetDisplay = false;
 
+function add(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply (a, b) {
+    return a*b;
+}
+
+function divide (a, b) {
+ if(b===0) {
+    return "Error;"
+ }
+ return a/b;
+}
