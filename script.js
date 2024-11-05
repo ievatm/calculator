1d98b1c1d16dc204c82f30550e9f1f4533d62d63
“ievatm” <Ieva.atmine@gmail.com>
2024-11-05 11:18:49 +0100

    operation function needed to create calculation by entered numbers

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function divide (a, b) {
  }
  return a/b;
 }
+
+function operate (operator, num1, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+        default:
+            return null;
+    }
+}
